ed01d8105b0afee4da4def7e5194010d1112a6bf
ekzyis <r.gill@stud.uni-heidelberg.de>
2018-11-23 00:09:26 +0100

    close https://github.com/ekzyis/physicsbot/issues/7

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+        {
+            name: "physicsbot",
+            script: "npm -- start",
+            out: "./physicsbot.log",
+            error: "./physicsbot.log"
+        },
+        {
+            name: "physicstest",
+            script: "npm -- run dev",
+            out: "./physicstest.log",
+            error: "./physicstest.log"
+        }
+    ]
+};
