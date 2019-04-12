2279030c004f8645efa707f41ba5fe851da71e90
ekzyis <r.gill@stud.uni-heidelberg.de>
2019-04-12 04:20:30 +0200

    Added config file to repo - changed npm scripts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,13 @@ module.exports = {
   apps: [
     {
       name: "physicsbot",
-      script: "npm -- run pistart",
+      script: "npm -- run start",
       out: "~/physicsbot/physicsbot.log",
       error: "~/physicsbot/physicsbot.log"
     },
     {
       name: "physicstest",
-      script: "npm -- run pidev",
+      script: "npm -- run dev",
       out: "~/physicsbot/physicstest.log",
       error: "~/physicsbot/physicstest.log"
     }
