f28db1aff399eb0e6534f24c7308fb47c80b9a3e
ekzyis <r.gill@stud.uni-heidelberg.de>
2019-04-15 00:30:56 +0200

    Added tests for genRoleNameMap

---
+++ test/testGenRoleNameMap.js
@@ -0,0 +1,62 @@
+import chai from "chai";
+import discord from "discord.js";
+import { genRoleNameMap } from "../src/gen.js";
+import fs from "fs";
+
+const expect = chai.expect;
+
+describe("genRoleNameMap should return a valid map according to config", function() {
+  this.timeout(15000);
+  let client;
+  before(function(done) {
+    client = new discord.Client();
+    client
+      .login(process.env.TOKEN)
+      .then(() => done())
+      .catch(console.error);
+  });
+
+  const GET_ENV_DESCRIBE = env =>
+    describe(`${env} environment`, function() {
+      let config, roleNameMap;
+      before(function() {
+        process.env.NODE_ENV = env;
+        config = JSON.parse(fs.readFileSync(process.env.CONFIG, "utf8"))[
+          process.env.NODE_ENV
+        ];
+        let guild = client.guilds.get(config.guild.id);
+        roleNameMap = genRoleNameMap(config, guild);
+      });
+
+      it("all roles listed in config with an emoji should be in the map", function() {
+        expect(
+          config.roles
+            .filter(item => item.emoji)
+            .map(item => item.role.id)
+            .every(id =>
+              Array.from(roleNameMap.values()).some(v => v.role.id === id)
+            )
+        ).to.equal(true);
+      });
+      it("the keys should map to the object defined in the config", function() {
+        expect(
+          config.roles
+            .filter(item => item.emoji)
+            .every(item => {
+              let mappedObject = roleNameMap.get(item.name);
+              return mappedObject.role.id === item.role.id &&
+                mappedObject.emoji.id === item.emoji.id &&
+                item.channel
+                ? mappedObject.channel.id === item.channel.id
+                : true;
+            })
+        );
+      });
+    });
+  GET_ENV_DESCRIBE("development");
+  GET_ENV_DESCRIBE("production");
+
+  after(function(done) {
+    client.destroy().then(done);
+  });
+});
