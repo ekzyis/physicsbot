7b7d543b6c95f6a1af68816b6b2d927dbf613d92
ekzyis <r.gill@stud.uni-heidelberg.de>
2019-04-14 23:59:59 +0200

    Added tests for genServerInstance

---
+++ test/testGenServerInstance.js
@@ -0,0 +1,88 @@
+import chai from "chai";
+import discord from "discord.js";
+import { genServerInstance } from "../src/gen.js";
+import fs from "fs";
+
+const expect = chai.expect;
+
+describe("genServerInstance should return a valid instance according to config", function() {
+  this.timeout(15000);
+  let client;
+  before(function(done) {
+    client = new discord.Client();
+    client
+      .login(process.env.TOKEN)
+      .then(() => done())
+      .catch(console.error);
+  });
+
+  describe("development environment", function() {
+    let instance, config;
+    before(function() {
+      process.env.NODE_ENV = "development";
+      config = JSON.parse(fs.readFileSync(process.env.CONFIG, "utf8"))[
+        process.env.NODE_ENV
+      ];
+      instance = genServerInstance(config, client);
+    });
+
+    const getConfigGuild = () => client.guilds.get(config.guild.id);
+    const getConfigChannel = id => getConfigGuild().channels.get(id);
+
+    it("guild is properly set", function() {
+      let guild = getConfigGuild();
+      expect(instance.guild).to.equal(guild);
+    });
+    it("default channel is properly set", function() {
+      let defaultChannel = getConfigChannel(config.channel.default.id);
+      expect(instance.defaultChannel).to.equal(defaultChannel);
+    });
+    it("rules channel is properly set", function() {
+      let rulesChannel = getConfigChannel(config.channel.rules.id);
+      expect(instance.rulesChannel).to.equal(rulesChannel);
+    });
+    it("overview channel is properly set", function() {
+      let overviewChannel = getConfigChannel(config.channel.overview.id);
+      expect(instance.overviewChannel).to.equal(overviewChannel);
+    });
+    it("dev channel is properly set", function() {
+      let devChannel = getConfigChannel(config.channel.dev.id);
+      expect(instance.devChannel).to.equal(devChannel);
+    });
+  });
+
+  describe("production environment", function() {
+    let instance, config;
+    before(function() {
+      process.env.NODE_ENV = "production";
+      config = JSON.parse(fs.readFileSync(process.env.CONFIG, "utf8"))[
+        process.env.NODE_ENV
+      ];
+      instance = genServerInstance(config, client);
+    });
+
+    const getConfigGuild = () => client.guilds.get(config.guild.id);
+    const getConfigChannel = id => getConfigGuild().channels.get(id);
+
+    it("guild is properly set", function() {
+      let guild = getConfigGuild();
+      expect(instance.guild).to.equal(guild);
+    });
+    it("default channel is properly set", function() {
+      let defaultChannel = getConfigChannel(config.channel.default.id);
+      expect(instance.defaultChannel).to.equal(defaultChannel);
+    });
+    it("rules channel is properly set", function() {
+      let rulesChannel = getConfigChannel(config.channel.rules.id);
+      expect(instance.rulesChannel).to.equal(rulesChannel);
+    });
+    it("overview channel is properly set", function() {
+      let overviewChannel = getConfigChannel(config.channel.overview.id);
+      expect(instance.overviewChannel).to.equal(overviewChannel);
+    });
+  });
+
+  after(function(done) {
+    client.destroy().then(() => done());
+  });
+});
