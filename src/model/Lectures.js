efe0b528b0e724d687b0d16a96f31a21a4beffb3
ekzyis <r.gill@stud.uni-heidelberg.de>
2019-04-30 23:11:18 +0200

    Added TODO for default value in schema

--- src/model/Lectures.js
+++ src/model/Lectures.js
@@ -7,6 +7,7 @@ const LectureSchema = mongoose.Schema(
     color: { type: String, default: null },
     updates: [
       {
+        // TODO Check if default value is really date of insertion and not time of compilation of this code
         time: { type: Date, default: new Date().getUTCDate() },
         scrape: { type: [{}] },
         notification: {
