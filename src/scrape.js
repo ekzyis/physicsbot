af8b072f7c258d0522d8c4d3da5c8dc4996f929f
ekzyis <r.gill@stud.uni-heidelberg.de>
2019-05-09 01:22:43 +0200

    Files are now downloaded and attached to embeds!

--- src/scrape.js
+++ src/scrape.js
@@ -98,52 +98,133 @@ export const PEP2_UPDATE = bot => async () => {
   handleUpdate(bot)(PEP2_LECTURE_NAME, scrape, $.root().html());
 };
 
-const handleUpdate = bot => (DB_LECTURE_NAME, scrape, html) => {
-  Lecture.findOne({ name: DB_LECTURE_NAME }, "updates color channel", function(
-    err,
-    lec
-  ) {
-    if (err) throw err;
-    if (lec.updates.length > 0) {
-      // there is at least one element already in updates!
-      // NOTE should checking for difference be more sophisticated or is this enough?
-      let diff =
-        scrape.length - lec.updates[lec.updates.length - 1].scrape.length;
-      if (diff > 0) {
-        // a previously not scraped element is found!
-        // send notification!
-        let channel = bot.guild.channels.get(lec.channel);
-        let title = `**${DB_LECTURE_NAME}: Neues Blatt!**`;
-        let description = scrape
-          .slice(diff)
-          .map((el, i) => `${el.text}\n${el.href}\n\n`)
-          .join("");
-        const embed = new discord.RichEmbed()
-          .setTitle(title)
-          .setDescription(description)
-          .setColor(lec.color);
-        channel
-          .send(embed)
-          .then(log(SEND_MESSAGE))
-          .catch(log(ERROR));
-        // save new update in document
-        lec.updates.push({
-          scrape,
-          html,
-          notification: {
-            title,
-            description
-          }
-        });
-        lec.save();
-        log(DB)(`Updated ${DB_LECTURE_NAME}`);
+const areEqual = (item1, item2) =>
+  item1.text === item2.text && item1.href === item2.href;
+
+const addedDiff = (oldScraped, newScraped) =>
+  newScraped.filter(
+    newItem => !oldScraped.some(oldItem => areEqual(oldItem, newItem))
+  );
+
+const removedDiff = (oldScraped, newScraped) =>
+  oldScraped.filter(
+    oldItem => !newScraped.some(newItem => areEqual(oldItem, newItem))
+  );
+
+const download = async (item, lectureName) => {
+  let cookieJar = null;
+  if (item.href.startsWith(MOODLE_URL)) {
+    cookieJar = await moodle_login();
+  }
+  return req({
+    url: item.href,
+    followAllRedirects: true,
+    jar: cookieJar,
+    encoding: null
+  })
+    .then(res => {
+      let dateFileNameFormat = new Date()
+        .toISOString()
+        .replace(/\..+/, "")
+        .replace(":", "")
+        .replace("-", "");
+      let filename = `${lectureName.replace(" ", "_")}_${item.href
+        .split("/")
+        .slice(-1)}_${dateFileNameFormat}`;
+      if (!fs.existsSync("download")) {
+        fs.mkdirSync("download");
       }
-      log(DB)(`No update for ${DB_LECTURE_NAME}`);
-    } else {
-      log(DB)(`First time scraped data saved for ${DB_LECTURE_NAME}`);
-      // first time saving state of website
-      lec.updates.push({ scrape, html });
-      lec.save();
+      fs.writeFileSync(`download/${filename}`, res.body);
+      return `download/${filename}`;
+    })
+    .catch(log(ERROR));
+};
+
+const handleUpdate = bot => (DB_LECTURE_NAME, scrape, html) => {
+  Lecture.findOne(
+    { name: DB_LECTURE_NAME },
+    "updates color channel",
+    async function(err, lec) {
+      if (err) throw err;
+      if (lec.updates.length > 0) {
+        // there is at least one element already in updates!
+        // NOTE should checking for difference be more sophisticated or is this enough?
+        let previouslyScraped = lec.updates[lec.updates.length - 1].scrape;
+        let added = addedDiff(previouslyScraped, scrape);
+        let removed = removedDiff(previouslyScraped, scrape);
+        if (added.length !== 0) {
+          // a previously not scraped element is found!
+          // download material
+          let files = await Promise.all(
+            added.map(item => download(item, DB_LECTURE_NAME))
+          );
+          // send notification!
+          let channel = bot.guild.channels.get(lec.channel);
+          let title = `**${DB_LECTURE_NAME}: Neues Blatt!**`;
+          let description = added
+            .map((el, i) => `${el.text}\n${el.href}\n\n`)
+            .join("");
+          let attachments = files.map((f, i) => ({
+            name: `${DB_LECTURE_NAME}_${scrape[i].text}.pdf`,
+            attachment: f
+          }));
+          const embed = new discord.RichEmbed()
+            .setTitle(title)
+            .setDescription(description)
+            .setColor(lec.color)
+            .attachFiles(attachments);
+          channel
+            .send(embed)
+            .then(log(SEND_MESSAGE))
+            .catch(log(ERROR));
+          // save new update in document
+          lec.updates.push({
+            scrape,
+            html,
+            notification: {
+              title,
+              description
+            }
+          });
+          lec.save();
+          log(DB)(`Updated ${DB_LECTURE_NAME}`);
+        }
+        if (removed.length !== 0) {
+          let logMessage = `Following items have been removed for ${DB_LECTURE_NAME}: ${removed}`;
+          // Write superuser a direct message since this should not happen!
+          let config = JSON.parse(fs.readFileSync(process.env.CONFIG, "utf8"))[
+            "development"
+          ];
+          let superUserRoleId = config.roles.find(r => r.name === "Superuser")
+            .role.id;
+          let superuser = bot.guild.members.find(member =>
+            member.roles.some(r => r.id === superUserRoleId)
+          );
+          superuser
+            .createDM()
+            .then(dmChannel => dmChannel.send(logMessage))
+            .catch(log(ERROR));
+          log(DB)(logMessage);
+          // save new update in document
+          lec.updates.push({
+            scrape,
+            html,
+            notification: {
+              title: null,
+              description: null
+            }
+          });
+          lec.save();
+          log(DB)(`Updated ${DB_LECTURE_NAME}`);
+        } else log(DB)(`No update for ${DB_LECTURE_NAME}`);
+      } else {
+        log(DB)(`First time scraped data saved for ${DB_LECTURE_NAME}`);
+        // download data
+        scrape.map(item => download(item, DB_LECTURE_NAME));
+        // first time saving state of website
+        lec.updates.push({ scrape, html });
+        lec.save();
+      }
     }
-  });
+  );
 };
