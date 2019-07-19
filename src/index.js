3a2b706e141438ae2fb82820d5d955ead57fb18e
ekzyis <r.gill@stud.uni-heidelberg.de>
2019-07-20 01:09:47 +0200

    Prettier added semicolon

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ client.on("ready", () => {
   client.user
     .setActivity("LHC live stream", { type: "WATCHING" })
     .catch(log(ERROR));
-})
+});
 
 client.once("ready", () => {
   const bot = new BotClient(client);
