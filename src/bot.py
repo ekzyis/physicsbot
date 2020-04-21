f65793afa54d7b727f8acef0620997ce6cef5dc3
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-04-21 23:20:46 +0200

    Added greeting function

--- src/bot.py
+++ src/bot.py
@@ -13,3 +13,13 @@ class BotClient(discord.Client):
     async def on_ready(self):
         """Executed when bot is logged in and ready."""
         print('Logged in as %s with id %s' % (self.user.name, self.user.id))
+
+    @staticmethod
+    async def on_member_join(member):
+        """Greets new member."""
+        guild = member.guild
+        if guild.system_channel is not None:
+            greeting = discord.Embed(
+                title="{}, willkommen auf {}!".format(str(member), str(guild))
+            )
+            await guild.system_channel.send(greeting)
