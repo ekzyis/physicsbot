dd9dbb04621e332c34b39061501a89d40115caec
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-04-22 12:13:39 +0200

    Added basic function create_lecture_embed

--- src/util.py
+++ src/util.py
@@ -1,3 +1,6 @@
+from discord import Embed
+
+
 def get_lecture_embed_message(channel, lecture):
     """Returns the message with the lecture embed if it exists in channel else None."""
     for message in channel.history(limit=20):
@@ -5,3 +8,9 @@ def get_lecture_embed_message(channel, lecture):
             if embed.title == lecture['embed_title']:
                 return message
     return None
+
+
+def create_lecture_embed(lecture):
+    return Embed(
+        title=lecture['embed_title']
+    )
