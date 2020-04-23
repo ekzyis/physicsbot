796978320647a668e84e71836e65c0660f58446a
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-04-23 02:44:07 +0200

    Bot can now remove roles via reaction removals

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,7 @@ from collections import namedtuple
 
 import discord
 
-from util import get_lecture_embed_message, create_lecture_embed, add_role_to_member
+from util import get_lecture_embed_message, create_lecture_embed, add_role_to_member, remove_role_from_member
 
 logging.basicConfig(level=logging.INFO)
 
@@ -41,6 +41,20 @@ class BotClient(discord.Client):
                 lecture_role_id = lecture['role']
                 member = raw_reaction.member
                 await add_role_to_member(member, lecture_role_id)
+
+    # TODO this code is very similar to the one in `on_raw_reaction_add`
+    async def on_raw_reaction_remove(self, raw_reaction):
+        """Handles users removing reactions from messages.
+        If an user removed his previous reaction from a lecture embed, the associated role is removed."""
+        # check if the reaction belongs to an lecture embed
+        lecture = self.get_lecture_of_message_id(raw_reaction.message_id)
+        if lecture is not None:
+            # check if reaction was the one we expect to assign the role
+            if raw_reaction.emoji.name == '\u2705':  # \u2705 is :white_check_mark:
+                lecture_role_id = lecture['role']
+                guild = self.get_guild(raw_reaction.guild_id)
+                member = guild.get_member(raw_reaction.user_id)
+                await remove_role_from_member(member, lecture_role_id)
 
     def get_lecture_of_message_id(self, message_id):
         """Returns the lecture associated with this message if there is one. Else returns None."""
--- src/util.py
+++ src/util.py
@@ -8,6 +8,13 @@ async def add_role_to_member(member, role_id):
     await member.add_roles(role)
 
 
+async def remove_role_from_member(member, role_id):
+    """Removes the role given by its id from the member.
+    The role id must be a from a role of the guild of the member (if that's not obvious)"""
+    role = member.guild.get_role(int(role_id))
+    await member.remove_roles(role)
+
+
 async def get_lecture_embed_message(channel, lecture):
     """Returns the message with the lecture embed if it exists in channel else None."""
     async for message in channel.history(limit=20):
