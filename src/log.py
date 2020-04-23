9936f81469093e359e7bb7ae0cf02e478e1b9299
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-04-23 12:32:09 +0200

    Added optional TODO

--- src/log.py
+++ src/log.py
@@ -5,6 +5,7 @@ def init_logger():
     """Initializes the discord logger and other loggers."""
     d_logger = logging.getLogger('discord')
     d_logger.setLevel(logging.DEBUG)
+    # TODO cmd option to clear log on startup?
     d_handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='a')
     d_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
     d_logger.addHandler(d_handler)
