0a0514260a58d74df570d35170f20b50b5189f06
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-04-23 05:09:25 +0200

    Better description for overview info embed

--- src/util.py
+++ src/util.py
@@ -42,7 +42,22 @@ async def create_overview_info_embed(guild, lectures):
     """Creates the info embed in the overview channel.
     The guild instance is needed for fetching roles and emojis."""
     # create embed description
-    desc = ""
+    desc = """Willkommen im **Rollen-Verteiler**.
+
+    Hier könnt ihr angeben was ihr studiert und Kurse "belegen" \
+    in dem ihr auf die Nachrichten unter dieser Nachricht *reagiert*.
+
+    Das ganze dient zur Übersicht und schaltet für die einzelnen Kurse bestimmte Text[- und Sprach]kanäle frei, \
+    die ihr jetzt noch nicht sehen könnt.
+
+    Reagieren ist ganz einfach: Klickt einfach auf die grün-weißen Häckchen bei der Rolle, die ihr haben \
+    wollt. Dadurch erscheinen neue Textkanäle, in denen du dich mit deinen Kommilitonen austauschen kannst. \
+    Dies ist auch reversibel; das heißt ihr könnt hier auch eine Rolle durch einfaches Klicken wieder entfernen, \
+    um z.B. über LA oder ANA nicht mehr informiert zu werden bzw. diese Kanäle nicht mehr zu sehen.
+
+    Hier ist eine kurze Übersicht über alle Rollen, die es gibt:
+
+    """
     for lecture in lectures:
         # TODO According to discord.py docs, I should iterate over emojis instead of using `fetch_emoji`
         # I'm doing it anyway because I think only having to do it once on startup is not that big of an issue.
