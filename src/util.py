079db3a530e9dfcce0d3165e6a56558ca6813ac0
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-04-23 03:19:12 +0200

    Fix same typo here
    And I thought I made sure I didn't copy the error lol

--- src/util.py
+++ src/util.py
@@ -10,7 +10,7 @@ async def add_role_to_member(member, role_id):
 
 async def remove_role_from_member(member, role_id):
     """Removes the role given by its id from the member.
-    The role id must be a from a role of the guild of the member (if that's not obvious)"""
+    The role id must be from a role of the guild of the member (if that's not obvious)"""
     role = member.guild.get_role(int(role_id))
     await member.remove_roles(role)
 
