097825fb37d0d25a912365b9476d7e2e35ee8f9f
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-04-25 13:15:27 +0200

    Added test case to check return type of create_lecture_embed

--- test/test_create_lecture_embed.py
+++ test/test_create_lecture_embed.py
@@ -1,6 +1,8 @@
 import unittest
 from unittest import mock
 
+import discord
+
 # noinspection PyUnresolvedReferences
 import test.context
 from src.util import create_lecture_embed
@@ -16,3 +18,12 @@ class TestCreateLectureEmbed(unittest.TestCase):
         embed = create_lecture_embed(guild, lecture)
         self.assertEqual(embed.title, 'title')
         self.assertIn('ROLE', embed.description)
+
+    @mock.patch('discord.Role')
+    @mock.patch('discord.Guild')
+    def test_create_lecture_embed_returns_embed(self, guild, role):
+        lecture = {'role': '12345', 'embed_title': 'title'}
+        role.mention = 'ROLE'
+        guild.get_role.return_value = role
+        embed = create_lecture_embed(guild, lecture)
+        self.assertIsInstance(embed, discord.Embed)
