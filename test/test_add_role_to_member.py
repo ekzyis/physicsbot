c75b3adc821dcccaf5a2f262fe26230c3b5610f4
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-04-25 17:16:53 +0200

    Added tests for add_role_to_member

---
+++ test/test_add_role_to_member.py
@@ -0,0 +1,33 @@
+from unittest import mock
+
+import aiounittest
+from aiounittest import futurized
+
+# noinspection PyUnresolvedReferences
+import test.context
+from src.util import add_role_to_member
+
+
+class TestAddRoleToMember(aiounittest.AsyncTestCase):
+
+    @mock.patch('discord.Role')
+    @mock.patch('discord.Member')
+    @mock.patch('discord.Guild')
+    async def test_add_role_to_member_adds_role_to_member_with_given_role_id_as_string(self, guild, member, role):
+        member = futurized(member)
+        guild.get_role.return_value = role
+        member.guild = guild
+        member.add_roles = mock.Mock(futurized(None))
+        await add_role_to_member(member, '12345')
+        member.add_roles.assert_called_once_with(role)
+
+    @mock.patch('discord.Role')
+    @mock.patch('discord.Member')
+    @mock.patch('discord.Guild')
+    async def test_add_role_to_member_adds_role_to_member_with_given_role_id_as_int(self, guild, member, role):
+        member = futurized(member)
+        guild.get_role.return_value = role
+        member.guild = guild
+        member.add_roles = mock.Mock(futurized(None))
+        await add_role_to_member(member, 12345)
+        member.add_roles.assert_called_once_with(role)
