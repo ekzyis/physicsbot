48de3c63e26a32a06791a4b1f3be56014d265669
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-04-25 12:31:59 +0200

    Added test for create_lecture_embed

---
+++ test/test_create_lecture_embed.py
@@ -0,0 +1,19 @@
+from unittest import mock
+
+import aiounittest
+
+# noinspection PyUnresolvedReferences
+import test.context
+from src.util import create_lecture_embed
+
+
+class TestCreateLectureEmbed(aiounittest.AsyncTestCase):
+    @mock.patch('discord.Guild')
+    @mock.patch('discord.Role')
+    def test_create_lecture_embed_returns_correct_embed(self, guild, role):
+        lecture = {'role': '12345', 'embed_title': 'title'}
+        role.mention = 'ROLE'
+        guild.get_role.return_value = role
+        embed = create_lecture_embed(guild, lecture)
+        self.assertEqual(embed.title, 'title')
+        self.assertIn('ROLE', embed.description)
