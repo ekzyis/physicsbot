16617892775180e8ce08cc08ff3b816845ea476f
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-04-25 19:22:43 +0200

    Added test case for greeting

---
+++ test/bot/test_greets_member.py
@@ -0,0 +1,26 @@
+from unittest import mock
+
+import aiounittest
+from aiounittest import futurized
+from discord import Embed
+
+# noinspection PyUnresolvedReferences
+import test.context
+from src.bot import BotClient
+
+
+class TestGreetsMember(aiounittest.AsyncTestCase):
+
+    def setUp(self):
+        self.bot = BotClient()
+
+    @mock.patch('discord.Guild')
+    @mock.patch('discord.Member')
+    async def test_greets_member_with_embed_if_guild_has_system_channel(self, member, guild):
+        guild.system_channel.send = mock.Mock(futurized(None))
+        member.guild = guild
+        await self.bot.on_member_join(member)
+        guild.system_channel.send.assert_called_once()
+        # first argument of first call
+        embed = guild.system_channel.send.call_args[0][0]
+        self.assertIsInstance(embed, Embed)
