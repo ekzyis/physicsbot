fb123a6d67964de3c3a1098454288372f66f3330
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-04-26 11:45:37 +0200

    Mocking bot client now in test_on_raw_reaction_add

--- test/bot/test_on_raw_reaction_add.py
+++ test/bot/test_on_raw_reaction_add.py
@@ -5,7 +5,6 @@ from aiounittest import futurized
 
 # noinspection PyUnresolvedReferences
 import test.context
-from src.bot import BotClient
 from src.const import WHITE_CHECK_MARK
 from src.event.on_raw_reaction_add import on_raw_reaction_add
 
@@ -13,18 +12,10 @@ from src.event.on_raw_reaction_add import on_raw_reaction_add
 class TestOnRawReactionAdd(aiounittest.AsyncTestCase):
 
     @classmethod
-    @mock.patch('discord.Client')
-    def setUpClass(cls, client):
-        """
-        According to https://stackoverflow.com/a/39307310, this does not work because when importing,
-        the definition of BotClient including the inheritance is executed.
-        """
-        user = mock.Mock()
-        user.id = '00000'
-        client.user = user
-        """Using the import here didn't work either. Don't know why."""
-        # from src.bot import BotClient
-        cls.bot = BotClient()
+    @mock.patch('src.bot.BotClient')
+    def setUpClass(cls, bot):
+        bot.user.id = '00000'
+        cls.bot = bot
 
     @mock.patch('discord.Member')
     @mock.patch('discord.Emoji')
@@ -32,12 +23,6 @@ class TestOnRawReactionAdd(aiounittest.AsyncTestCase):
     async def test_on_raw_reaction_add_adds_role_when_reacted_with_white_check_mark_on_lecture_embed(
             self, reaction, emoji, member
     ):
-        """
-        Fails with
-            AttributeError: 'NoneType' object has no attribute 'id'
-        because the bot is not logged in thus there is no user.
-        See https://discordpy.readthedocs.io/en/latest/api.html#discord.Client.user
-        """
         self.bot.lecture_message_tuples = [({'role': '1234'}, '5678')]
         member.add_roles = mock.Mock(futurized(None))
         emoji.name = WHITE_CHECK_MARK
