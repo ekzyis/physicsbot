c9b05de583a2927364a33ecc5bc9861d4aff53bf
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-04-27 12:16:56 +0200

    Added setUp which configures mocks

--- test/bot/test_on_raw_reaction_add.py
+++ test/bot/test_on_raw_reaction_add.py
@@ -21,25 +21,40 @@ class TestOnRawReactionAdd(aiounittest.AsyncTestCase):
     @mock.patch('discord.Member')
     @mock.patch('discord.Emoji')
     @mock.patch('discord.RawReactionActionEvent')
-    async def test_on_raw_reaction_add_adds_role_when_reacted_with_white_check_mark_on_lecture_embed(
-            self, reaction, emoji, member, role
-    ):
+    def setUp(self, reaction, emoji, member, role):
         # setup the lecture mock we will receive when calling `get_lecture_of_message_id`
         lecture_mock = mock.MagicMock()
         # when calling lecture['role'], we want to get the "role id"
         lecture_mock.__getitem__.return_value = '1234'
         # bot#get_lecture_of_message_id should return the mocked lecture
         self.bot.get_lecture_of_message_id.return_value = lecture_mock
-        # member#add_roles should be awaitable
+        """member#add_roles should always be awaitable even though we are not always expecting that this function will
+        be called. The reason for this that we don't want to couple our test to tightly with the code; expecting more
+        from the SUT (system under test) than we are testing.
+        We assert that it was or was not alled but don't want to throw an error during testing just because it
+        is not awaitable. The actual reason to fail the test should be that it was or was not called!
+        """
         member.add_roles = mock.Mock(futurized(None))
-        # guild#get_role should return the role we want to add to the member
+        # guild#get_role should return the mocked role
         member.guild.get_role.return_value = role
-        # user reacted with WHITE_CHECK_MARCK
-        emoji.name = WHITE_CHECK_MARK
-        # "populate" the reaction we will pass to bot#on_raw_reaction_add
+        # add mocks to reaction
         reaction.member = member
         reaction.emoji = emoji
+        # add mocks to instance so we can access them in the test cases
+        self.reaction = reaction
+        self.emoji = emoji
+        self.member = member
+        self.role = role
+        self.lecture_mock = lecture_mock
+
+    async def test_on_raw_reaction_add_adds_role_when_reacted_with_white_check_mark_on_lecture_embed(self):
+        reaction, emoji, member, role, lecture_mock = \
+            (self.reaction, self.emoji, self.member, self.role, self.lecture_mock)
+        # user reacted with WHITE_CHECK_MARCK
+        emoji.name = WHITE_CHECK_MARK
+        # user reacted to message with message id 5678
         reaction.message_id = '5678'
+        # it was an actual user which reacted, not our bot
         reaction.user_id = '11111'  # not equal to bot.user.id
 
         await on_raw_reaction_add(self.bot)(reaction)
@@ -63,12 +78,6 @@ class TestOnRawReactionAdd(aiounittest.AsyncTestCase):
         lecture_mock = mock.MagicMock()
         lecture_mock.__getitem__.return_value = '1234'
         self.bot.get_lecture_of_message_id.return_value = lecture_mock
-        """make member#add_roles awaitable again even though we are expecting that this function will not be called.
-        The reason for this that we don't want to couple our test to tightly with the code; expecting more from the
-        SUT (system under test) than we are testing.
-        We will later assert that it was not called but don't want to throw an error during testing just because it
-        is not awaitable. The actual reason to fail the test should be that it was called!
-        """
         member.add_roles = mock.Mock(futurized(None))
         # same reasoning as with member#add_roles
         member.get_guild.return_value = role
