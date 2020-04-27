7973ff080d19650618c81cdc6eb80ee7156840ea
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-04-27 19:43:46 +0200

    Fix test case naming

--- test/bot/test_on_raw_reaction_remove.py
+++ test/bot/test_on_raw_reaction_remove.py
@@ -7,7 +7,7 @@ import test.context
 from src.event.on_raw_reaction_remove import on_raw_reaction_remove
 
 
-class TestOnRawReactionAdd(aiounittest.AsyncTestCase):
+class TestOnRawReactionRemove(aiounittest.AsyncTestCase):
 
     @mock.patch('discord.Role', autospec=True)
     @mock.patch('discord.Member', autospec=True)
