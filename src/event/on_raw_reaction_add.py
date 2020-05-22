982267f63ddae7271d772d62b0acb169ec4ee537
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-22 23:15:58 +0200

    Add missing type annotations

--- src/event/on_raw_reaction_add.py
+++ src/event/on_raw_reaction_add.py
@@ -1,8 +1,9 @@
-from typing import Callable, Coroutine, TYPE_CHECKING
+from typing import Callable, Coroutine, TYPE_CHECKING, Optional
 
 import discord
 
 from const import WHITE_CHECK_MARK
+from util.lecture import Lecture
 from util.member import add_role_to_member
 
 if TYPE_CHECKING:
@@ -27,10 +28,10 @@ def on_raw_reaction_add(bot: 'BotClient') -> Callable[[discord.RawReactionAction
         # check if reaction was the one we expect to for role assignment
         if emoji == WHITE_CHECK_MARK:
             # check if the reaction belongs to an lecture embed
-            lecture = bot.get_lecture_of_message_id(message_id)
+            lecture: Optional[Lecture] = bot.get_lecture_of_message_id(message_id)
             if lecture is not None:
                 bot.logger.info('Reaction was added to embed of lecture %s' % lecture.embed_title)
-                lecture_role_id = lecture.role
+                lecture_role_id: str = lecture.role
                 await add_role_to_member(member, lecture_role_id)
                 bot.logger.info("Role added!")
 
