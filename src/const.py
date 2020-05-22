8e435d3dc022ec13c84ef418922eb782e0fcb335
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-22 21:35:26 +0200

    Type annotate const

--- src/const.py
+++ src/const.py
@@ -3,4 +3,4 @@ This file is used to score constants such as the unicode characters of reactions
 For example, the unicode character for :white_check_mark: is \u2705.
 """
 
-WHITE_CHECK_MARK = '\u2705'
+WHITE_CHECK_MARK: str = '\u2705'
