96aef5a6e1b020e93fc3e05d025dc447d1db7285
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-27 22:16:15 +0200

    Restructed test_command_reactionmessage_remove

--- test/bot/reactionmessages/test_command_reactionmessage_remove.py
+++ test/bot/reactionmessages/test_command_reactionmessage_remove.py
@@ -8,11 +8,6 @@ import test.context
 from command.reactionmessage import ReactionMessage, reactionmessage_remove
 
 
-@mock.patch('discord.Emoji', autospec=True)
-@mock.patch('discord.Role', autospec=True)
-@mock.patch('discord.Message', autospec=True)
-@mock.patch('src.bot.BotClient', autospec=True)
-@mock.patch('discord.ext.commands.Context')
 class TestCommandReactionMessageAdd(aiounittest.AsyncTestCase):
     """Test class for the function which is called when command `!reactionmessage add` is recognized.
 
@@ -23,25 +18,39 @@ class TestCommandReactionMessageAdd(aiounittest.AsyncTestCase):
         - error handling
     """
 
-    async def test_command_reactionmessage_remove_removes_ReactionMessage_to_bot_instance(self, ctx, bot, message, role,
-                                                                                          emoji):
+    @mock.patch('src.bot.BotClient', autospec=True)
+    @mock.patch('discord.ext.commands.Context')
+    def setUp(self, ctx, bot):
         ctx.bot = bot
+        ctx.channel.send = mock.Mock(futurized(None))
+        self.ctx, self.bot = ctx, bot
+
+    @mock.patch('discord.Emoji', autospec=True)
+    @mock.patch('discord.Role', autospec=True)
+    @mock.patch('discord.Message', autospec=True)
+    async def test_command_reactionmessage_remove_removes_ReactionMessage_to_bot_instance(self, message, role, emoji):
+        ctx, bot = self.ctx, self.bot
         await reactionmessage_remove(ctx, message, role, emoji)
         rm = ReactionMessage(mid=message.id, role=role, emoji=emoji)
         bot.remove_reactionmessage.assert_called_with(rm)
 
-    async def test_command_reactionmessage_remove_informs_user_if_successful(self, ctx, bot, message,
-                                                                             role,
-                                                                             emoji):
+    @mock.patch('discord.Emoji', autospec=True)
+    @mock.patch('discord.Role', autospec=True)
+    @mock.patch('discord.Message', autospec=True)
+    async def test_command_reactionmessage_remove_informs_user_if_successful(self, message, role, emoji):
+        ctx, bot = self.ctx, self.bot
         ctx.bot = bot
         await reactionmessage_remove(ctx, message, role, emoji)
         ctx.channel.send.assert_called_once()
 
-    async def test_command_reactionmessage_remove_replies_with_error_if_ReactionMessage_does_not_exit(self, ctx, bot,
-                                                                                                      message, role,
+    @mock.patch('discord.Emoji', autospec=True)
+    @mock.patch('discord.Role', autospec=True)
+    @mock.patch('discord.Message', autospec=True)
+    async def test_command_reactionmessage_remove_replies_with_error_if_ReactionMessage_does_not_exit(self, message,
+                                                                                                      role,
                                                                                                       emoji):
+        ctx, bot = self.ctx, self.bot
         ctx.bot = bot
-        ctx.channel.send = mock.Mock(futurized(None))
         bot.remove_reactionmessage.side_effect = mock.Mock(side_effect=ValueError)
         await reactionmessage_remove(ctx, message, role, emoji)
         rm = ReactionMessage(mid=message.id, role=role, emoji=emoji)
