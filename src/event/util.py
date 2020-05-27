a2c4f8431de3ba1119132b7cd0d8ee8e0e97d38e
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-27 20:21:25 +0200

    Fix typing

--- src/event/util.py
+++ src/event/util.py
@@ -7,19 +7,19 @@ if TYPE_CHECKING:
 
 
 def on_raw_reaction_add_arg_parser(raw_reaction: discord.RawReactionActionEvent) \
-        -> Tuple[discord.Member, discord.Emoji, int]:
+        -> Tuple[discord.Member, str, int]:
     """Parse the argument of on_raw_reaction_add listener and return member, emoji, message_id."""
     member: discord.Member = raw_reaction.member
-    emoji: discord.Emoji = raw_reaction.emoji.name  # NOTE This actually returns a PartialEmoji
+    emoji: str = raw_reaction.emoji.name  # NOTE This actually returns a PartialEmoji
     message_id: int = raw_reaction.message_id
     return member, emoji, message_id
 
 
 def on_raw_reaction_remove_arg_parser(raw_reaction: discord.RawReactionActionEvent, bot: 'BotClient') \
-        -> Tuple[discord.Member, discord.Emoji, int]:
+        -> Tuple[discord.Member, str, int]:
     """Parse the argument of on_raw_reaction_remove listener and return member, emoji, message_id."""
     guild: discord.Guild = bot.get_guild(raw_reaction.guild_id)
     member: discord.Member = guild.get_member(raw_reaction.user_id)
-    emoji: discord.Emoji = raw_reaction.emoji.name
+    emoji: str = raw_reaction.emoji.name
     message_id: int = raw_reaction.message_id
     return member, emoji, message_id
