3bdf3630e3baacd11bc13f4d32e1466ec19ee4a3
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-05-27 20:32:20 +0200

    Added test for command add_reactionmessage

--- test/bot/reactionmessages/test_command_add_reactionmessage.py
+++ test/bot/reactionmessages/test_command_add_reactionmessage.py
@@ -1,15 +1,23 @@
-import unittest
 from unittest import mock
 
 # noinspection PyUnresolvedReferences
+import aiounittest
+
+# noinspection PyUnresolvedReferences
 import test.context
+from command.reactionmessage import add_reactionmessage, ReactionMessage
 
 
-class TestCommandAddReactionMessage(unittest.TestCase):
+class TestCommandAddReactionMessage(aiounittest.AsyncTestCase):
 
     @mock.patch('discord.Emoji', autospec=True)
     @mock.patch('discord.Role', autospec=True)
     @mock.patch('discord.Message', autospec=True)
     @mock.patch('src.bot.BotClient', autospec=True)
-    def test_sending_command_message_adds_reaction_message_handler(self, bot, message, role, emoji):
-        pass
+    @mock.patch('discord.ext.commands.Context', autospec=True)
+    async def test_command_add_reactionmessage_adds_ReactionMessage_to_bot_instance(self, ctx, bot, message, role,
+                                                                                    emoji):
+        ctx.bot = bot
+        await add_reactionmessage(ctx, message, role, emoji)
+        rm = ReactionMessage(mid=message.id, role=role, emoji=emoji)
+        bot.add_reactionmessage.assert_called_with(rm)
